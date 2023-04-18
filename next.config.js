083a2c1876bd38fb7e2badd9ed3b84203102d5d3
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-04-18 21:44:11 +0500

    Add SQL

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
+  env: {
+    PGHOST: "ep-green-hill-676240.us-east-2.aws.neon.tech",
+    PGDATABASE: "neondb",
+    PGUSER: "ahmed.shaykhwork",
+    PGPASSWORD: "Br6zhJXo4qWG"
+  }
 };
 
 module.exports = nextConfig;
