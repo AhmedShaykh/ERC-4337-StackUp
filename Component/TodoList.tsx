435c286ed3728fd1deb0b24eaea2ad4bcc8b1101
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-05-24 14:02:48 +0500

    Add Add Todo Component

---
+++ Component/TodoList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Todos } from "@/lib/drizzle";
+
+const TodoList = () => {
+    return (
+        <div className="max-h-[350px] overflow-auto mb-4 flex items-center gap-x-3 shadow rounded-lg my-5">
+            {/* <div className="h-6 w-6 bg-black-900 rounded-full"></div>
+            <p className="text-lg font-medium">Hi</p> */}
+        </div>
+    )
+};
+
+export default TodoList;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import Todo from '@/Component/Todo';
+import TodoList from '@/Component/TodoList';
+import AddTodo from '@/Component/AddTodo';
 
 const page = () => {
     return (
-        <div className="flex flex-col items-center my-4">
-            <Todo />
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className="px-6 py-8 rounded-xl bg-gradient-to-br from-[#D9D9D9]/50 to-[#D9D9D9]/60  backdrop-blur-xl w-full max-w-md">
+                <TodoList />
+                <AddTodo />
+
+                <div className="w-1/2 h-1.5 bg-black/80 rounded mx-auto mt-6" />
+            </div>
         </div>
     )
 };
