e30ac989c4d17040d6f9c87ffb86133cb547a349
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-08-08 21:06:50 +0500

    Add API Routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ const page = () => {
         <div className="bg-gradient-to-b from-gray-900 to-blue-900 flex items-center justify-center h-screen">
             <div className="p-4 rounded-xl bg-gradient-to-br from-[#D9D9D9]/50 to-[#D9D9D9]/60 backdrop-blur-xl w-full max-w-md">
                 {/* @ts-ignore */}
-                <TodoList />
+                {/* <TodoList />
 
-                <AddTodo />
+                <AddTodo /> */}
                 <div className="w-1/2 h-1.5 bg-black/80 rounded mx-auto mt-4" />
             </div>
         </div>
