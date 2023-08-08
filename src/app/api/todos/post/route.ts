6727a4cab3a339c01994f3e55de610cc052a4a5a
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-08-09 00:24:43 +0500

    Add GET & POST API Route

--- src/app/api/todos/post/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-export const POST = async (request: NextRequest) => {
-
-    const body = await request.json();
-
-    return NextResponse.json({ message: "POST request successful", data: body });
-
-};
--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -5,3 +5,11 @@ export const GET = async (request: NextRequest) => {
     return NextResponse.json({ message: "GET request successful" });
 
 };
+
+export const POST = async (request: NextRequest) => {
+
+    const body = await request.json();
+
+    return NextResponse.json({ message: "POST request successful", data: body });
+
+};
